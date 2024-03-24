bef56ff3fdb09e07077552f09c4aeaf602c5bb3a
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-03-25 00:06:41 +0530

    Modified status code for POST request

--- src/main/java/com/example/TodoAPISpring/TodoController.java
+++ src/main/java/com/example/TodoAPISpring/TodoController.java
@@ -1,9 +1,7 @@
 package com.example.TodoAPISpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,6 +23,7 @@ public class TodoController {
     }
 
     @PostMapping("/todos")
+    @ResponseStatus(HttpStatus.CREATED)
     public Todo createTodo(@RequestBody Todo newTodo) {
         todoList.add(newTodo);
         return newTodo;
